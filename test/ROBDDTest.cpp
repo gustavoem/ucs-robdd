7e97f4e056624c4864f8193608997b38c3e3b5e4
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2015-02-15 20:23:59 -0200

    O deletador funcionou para todas as robdds testadas

--- test/ROBDDTest.cpp
+++ test/ROBDDTest.cpp
@@ -17,7 +17,7 @@ namespace ROBDDTest {
 	/*not a test*/
 	bool print_r () 
 	{
-		/*ElementSet * elm_set = new ElementSet ("", 3, 100);
+		ElementSet * elm_set = new ElementSet ("", 3, 100);
 		ROBDD * robdd = new ROBDD (elm_set);
 		robdd->print ();
 		cout.flush();
@@ -25,15 +25,15 @@ namespace ROBDDTest {
 		cout << "depois de reduzir" << endl;
 		robdd->print ();
 		cout << " fim da robdd 1 " << endl  << endl;
-		delete robdd;*/
+		delete robdd;
 		
-		/*ElementSubset * subset = new ElementSubset("", elm_set);
+		ElementSubset * subset = new ElementSubset("", elm_set);
 		subset->add_element(0);
 		subset->add_element(1);
 		subset->add_element(2);
 		ROBDD * robdd2 = new ROBDD (elm_set, subset);
 		robdd2->print ();
-		robdd2->reduce ();
+		// robdd2->reduce ();
 		cout << "depois de reduzir" << endl;
 		robdd2->print ();
 		cout << " fim da robdd 2. |G| = " << robdd2->cardinality << endl << endl;
@@ -43,13 +43,13 @@ namespace ROBDDTest {
 		ROBDD * robdd3 = new ROBDD ();
 		cout << "robdd 3" << endl;
 		robdd3->print ();
-		robdd3->reduce ();
+		// robdd3->reduce ();
 		cout << "depois de reduzir" << endl;
 		robdd3->print ();
 		cout << " fim da robdd 3. |G| = " << robdd3->cardinality << endl << endl;
 		delete robdd3;
 
-		delete elm_set;*/
+		delete elm_set;
 		return true;
 	}
 
